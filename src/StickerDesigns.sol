2e6d1d45a84f2902a4f5977f0a5942dcce236fca
ivyroot <ivyrootcode@gmail.com>
2024-02-16 23:14:11 -0800

    add OpenZeppelin contracts and implement Ownable

--- src/StickerDesigns.sol
+++ src/StickerDesigns.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
-
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "erc721a/contracts/ERC721A.sol";
 
-contract StickerDesigns is ERC721A {
-    constructor() ERC721A("StickerDesignz", "STKRS-TEST-DEV") {}
+contract StickerDesigns is ERC721A, Ownable {
+    constructor() ERC721A("StickerDesignz", "STKRS-TEST-DEV") Ownable(msg.sender) {}
 
     function mint(uint256 quantity) external payable {
         // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
