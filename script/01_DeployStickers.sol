da0847e0d815b455076b3c50fd67ee0b395d6c02
ivyroot <ivyrootcode@gmail.com>
2024-04-10 14:16:54 -0700

    add deployment script

---
+++ script/01_DeployStickers.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+import "../src/StickerDesigns.sol";
+
+
+contract DeployStickers is Script {
+    function run() external {
+        vm.startBroadcast();
+        new StickerDesigns(2000000000000000, 50000000000000);
+        vm.stopBroadcast();
+    }
+}
