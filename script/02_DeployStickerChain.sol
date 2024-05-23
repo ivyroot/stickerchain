7dce5ea8c406b1baf7ccf0222ba3e388791150f3
ivyroot <ivyrootcode@gmail.com>
2024-05-23 15:02:53 -0700

    initial addition of deploy script

---
+++ script/02_DeployStickerChain.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+
+import "../src/StickerDesigns.sol";
+
+
+contract DeployStickers is Script {
+    function run() external {
+        address blockPlacesContractAddress = vm.envAddress('BLOCK_PLACES_CONTRACT');
+        require(blockPlacesContractAddress != address(0), 'DeployStickers: BLOCK_PLACES_CONTRACT not set');
+        BlockPlaces blockPlaces = BlockPlaces(blockPlacesContractAddress);
+        require(blockPlaces.address != address(0), 'DeployStickers: BLOCK_PLACES_CONTRACT not set');
+        address stickerDesignsContractAddress = vm.envAddress('STICKER_DESIGNS_CONTRACT');
+        require(stickerDesignsContractAddress != address(0), 'DeployStickers: STICKER_DESIGNS_CONTRACT not set');
+        StickerDesigns stickerDesigns = StickerDesigns(stickerDesignsContractAddress);
+        require(stickerDesigns.owner() == address(tx.origin), 'DeployStickers: owner mismatch');
+        vm.startBroadcast();
+        new StickerDesigns(address(tx.origin), 2000000000000000, 50000000000000);
+        vm.stopBroadcast();
+    }
+}
