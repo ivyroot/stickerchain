063a124510613834ea24bbc75f60b73f902f35fa
ivyroot <ivyrootcode@gmail.com>
2024-05-30 15:44:59 -0700

    add tests for getting slaps at place

--- test/PlayerSlapTest.t.sol
+++ test/PlayerSlapTest.t.sol
@@ -12,6 +12,7 @@ contract PlayerSlapTest is Test {
     uint256 public newStickerFee = 0.0005 ether;
     uint256 public slapFee = 0.001 ether;
     uint256 private exampleStickerId1;
+    uint256 private placeIdUnionSquare = 7147618599;
     address address1 = address(0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97);
     address address2 = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     address address3 = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
@@ -44,10 +45,10 @@ contract PlayerSlapTest is Test {
     function testSlapOneSticker() public {
         vm.deal(address1, 20 ether);
         vm.startPrank(address1);
-        stickerChain.slap{value: slapFee}(7147618599, exampleStickerId1, 1);
+        stickerChain.slap{value: slapFee}(placeIdUnionSquare, exampleStickerId1, 1);
         Slap memory slap = stickerChain.getSlap(1);
         assertEq(slap.stickerId, exampleStickerId1);
-        assertEq(slap.placeId, 7147618599);
+        assertEq(slap.placeId, placeIdUnionSquare);
         assertEq(slap.player, address1);
         assertEq(slap.slappedAt, block.timestamp);
     }
@@ -55,13 +56,13 @@ contract PlayerSlapTest is Test {
     function testGetPlaceSlapsWithOneSticker() public {
         vm.deal(address1, 2 ether);
         vm.prank(address1);
-        stickerChain.slap{value: slapFee}(7147618599, exampleStickerId1, 1);
+        stickerChain.slap{value: slapFee}(placeIdUnionSquare, exampleStickerId1, 1);
         // load stickers from place
-        (uint total, Slap[] memory slaps) = stickerChain.getPlaceSlaps(7147618599, 0, 0);
+        (uint total, Slap[] memory slaps) = stickerChain.getPlaceSlaps(placeIdUnionSquare, 0, 0);
         assertEq(total, 1);
         assertEq(slaps[0].stickerId, exampleStickerId1);
         assertEq(slaps[0].player, address1);
-        assertEq(slaps[0].placeId, 7147618599);
+        assertEq(slaps[0].placeId, placeIdUnionSquare);
     }
 
     function testGetPlaceSlapsWithTwoStickers() public {
@@ -72,29 +73,41 @@ contract PlayerSlapTest is Test {
         uint timestamp1 = 1717108737;
         vm.warp(timestamp1);
         vm.prank(address1);
-        stickerChain.slap{value: slapFee}(7147618599, exampleStickerId1, 1);
+        stickerChain.slap{value: slapFee}(placeIdUnionSquare, exampleStickerId1, 1);
 
         // slap second sticker
         vm.roll(50 + block.number);
         vm.warp(2600 + block.timestamp);
         uint timestamp2 = block.timestamp;
         vm.prank(address2);
-        stickerChain.slap{value: slapFee}(7147618599, exampleStickerId1, 1);
+        stickerChain.slap{value: slapFee}(placeIdUnionSquare, exampleStickerId1, 1);
 
         // load stickers from place
-        (uint total, Slap[] memory slaps) = stickerChain.getPlaceSlaps(7147618599, 0, 0);
+        (uint total, Slap[] memory slaps) = stickerChain.getPlaceSlaps(placeIdUnionSquare, 0, 0);
         assertEq(total, 2);
 
         // check latest slap is most recent
         assertEq(slaps[0].stickerId, exampleStickerId1);
         assertEq(slaps[0].player, address2);
-        assertEq(slaps[0].placeId, 7147618599);
+        assertEq(slaps[0].placeId, placeIdUnionSquare);
         assertEq(slaps[0].slappedAt, timestamp2);
 
         assertEq(slaps[1].stickerId, exampleStickerId1);
         assertEq(slaps[1].player, address1);
-        assertEq(slaps[1].placeId, 7147618599);
+        assertEq(slaps[1].placeId, placeIdUnionSquare);
 
         assertEq(slaps[1].slappedAt, timestamp1);
+    }
+
+    function testCreateSlapOfSize2() public {
+        vm.deal(address1, 2 ether);
+        vm.prank(address1);
+        stickerChain.slap{value: slapFee}(placeIdUnionSquare, exampleStickerId1, 2);
+        // load stickers from place
+        (uint total, Slap[] memory slaps) = stickerChain.getPlaceSlaps(placeIdUnionSquare, 0, 0);
+        assertEq(total, 1);
+        assertEq(slaps[0].stickerId, exampleStickerId1);
+        assertEq(slaps[0].player, address1);
+        assertEq(slaps[0].placeId, placeIdUnionSquare);
     }
 }
