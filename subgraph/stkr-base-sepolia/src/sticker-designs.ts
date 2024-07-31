48c387437ecc2b34bc4121b18df869a4f4d0cd4c
ivyroot <ivyrootcode@gmail.com>
2024-07-30 21:26:03 -0500

    configure subgraph to pull sticker design meta from IPFS

--- subgraph/stkr-base-sepolia/src/sticker-designs.ts
+++ subgraph/stkr-base-sepolia/src/sticker-designs.ts
@@ -1,3 +1,4 @@
+import { json, Bytes, dataSource } from '@graphprotocol/graph-ts'
 import {
   AdminFeeRecipientChanged as AdminFeeRecipientChangedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
@@ -10,9 +11,10 @@ import {
   StickerRegistrationFeeChanged as StickerRegistrationFeeChangedEvent
 } from "../generated/StickerDesigns/StickerDesigns"
 import {
-  StickerDesign
+  StickerDesign,
+  StickerDesignMetadata
 } from "../generated/schema"
-
+import { StickerDesignMetadata as StickerDesignMetadataTemplate } from "../generated/templates"
 
 
 
@@ -25,11 +27,36 @@ export function handleStickerDesignPublished(
   entity.stickerId = event.params.stickerId
   entity.publisher = event.params.publisher
   entity.payoutAddress = event.params.payoutAddress
-  entity.metadataCID = event.params.metadataCID
+
+  if (event.params.metadataCID) {
+    entity.metadataCID = event.params.metadataCID.toHexString()
+    StickerDesignMetadataTemplate.create(entity.metadataCID)
+  }
 
   entity.save()
 }
 
 
 
+export function handleStickerDesignMetadata(content: Bytes): void {
+  let stickerMeta = new StickerDesignMetadata(dataSource.stringParam())
+  const value = json.fromBytes(content).toObject()
+  if (value) {
+    const imageCID = value.get('imageCID')
+    const filename = value.get('filename')
+    const contentType = value.get('contentType')
+    const aspectRatio = value.get('aspectRatio')
+    const size = value.get('size')
 
+    if (imageCID == null || filename == null || contentType == null || aspectRatio == null || size == null) {
+      return
+    }
+
+    stickerMeta.imageCID = imageCID.toString()
+    stickerMeta.filename = filename.toString()
+    stickerMeta.contentType = contentType.toString()
+    stickerMeta.aspectRatio = aspectRatio.toBigInt()
+    stickerMeta.size = size.toBigInt()
+    stickerMeta.save()
+  }
+}
