b659a862e51a671660e8c1b5868bdd5ffc401b6b
ivyroot <ivyrootcode@gmail.com>
2024-09-12 21:24:30 -0500

    update subgraph definition to include slap height

--- subgraph/stkr-base-sepolia/src/sticker-chain.ts
+++ subgraph/stkr-base-sepolia/src/sticker-chain.ts
@@ -4,6 +4,7 @@ import {
   ConsecutiveTransfer as ConsecutiveTransferEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   StickerSlapped as StickerSlappedEvent,
+  StickerChain as StickerChainContract,
   Transfer as TransferEvent,
 } from "../generated/StickerChain/StickerChain"
 import {
@@ -12,16 +13,20 @@ import {
 
 
 export function handleStickerSlapped(event: StickerSlappedEvent): void {
+
   let entity = new Slap(
     event.transaction.hash.concatI32(event.logIndex.toI32()),
   )
+  const contractInstance = StickerChainContract.bind(event.address)
+  const slap = contractInstance.getSlap(event.params.slapId)
   entity.slapId = event.params.slapId
   entity.placeId = event.params.placeId
+  entity.height = slap.height
   entity.stickerId = event.params.stickerId
   entity.player = event.params.player
   entity.owner = entity.player
   entity.size = event.params.size
-  entity.timestamp = event.block.timestamp
+  entity.slappedAt = event.block.timestamp
   entity.blockNumber = event.block.number
 
   entity.save()
